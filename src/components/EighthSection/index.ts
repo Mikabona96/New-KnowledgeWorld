c2882b7ee8b186a1c3391c43029379fae16b1185
Mikabona96 <76862391+Mikabona96@users.noreply.github.com>
2022-11-29 12:55:43 +0200

    8th section all resolutions done

--- src/components/EighthSection/index.ts
+++ src/components/EighthSection/index.ts
@@ -2,38 +2,103 @@ import '../../assets/img/8th-section-img.png';
 
 export const eighthSectionFunction = () => {
     const zoomBtn = (document.querySelector('.tapToZoom')) as HTMLElement;
-    const mapWrapper = (document.querySelector('.map')) as HTMLElement;
+    const map = (document.querySelector('.map')) as HTMLElement;
+    const mapWrapper = (document.querySelector('.map-wrapper')) as HTMLElement;
     const mapImg = (document.querySelector('.map img')) as HTMLElement;
     const body = (document.querySelector('body')) as HTMLElement;
-    const width = body.offsetWidth;
+    const eighthSection = (document.querySelector('.eighthsection')) as HTMLElement;
+    let top = mapWrapper.offsetTop;
+    let width = body.offsetWidth;
 
-    zoomBtn.addEventListener('click', () => {
-        body.style.overflow = 'hidden';
+    let mapWidth = window.getComputedStyle(map).width;
+
+    const zoomBtnClickHandler = () => {
+        // body.style.overflow = 'hidden';
         zoomBtn.style.display = 'none';
-        mapWrapper.style.position = 'absolute';
-        mapWrapper.style.top = '0';
-        mapWrapper.style.left = '0';
-        mapWrapper.style.overflow = 'scroll';
-        mapWrapper.style.marginTop = '0px';
-        mapWrapper.style.height = '100vh';
-        mapWrapper.style.filter = 'none';
+        map.style.position = 'absolute';
+        map.style.top = '0';
+        map.style.left = '0';
+        map.style.overflow = 'scroll';
+        map.style.marginTop = '0px';
+        mapWrapper.style.height = 'unset'; //
+        eighthSection.style.height = '990px'; //
+        map.style.height = '695px';
+        map.style.width = '100%'; //
+        map.style.filter = 'none';
         mapImg.style.left = '0';
         mapImg.style.width = '1263px';
         mapImg.style.height = '695px';
-    });
-    mapImg.addEventListener('click', () => {
-        body.style.overflow = 'scroll';
+        window.scrollTo({
+            top,
+            left:     0,
+            behavior: 'smooth',
+        });
+    };
+
+    const mapImgClickHandler = () => {
+        // body.style.overflow = 'scroll';
         zoomBtn.style.display = 'block';
-        mapWrapper.style.position = 'relative';
-        mapWrapper.style.top = '0';
-        mapWrapper.style.left = '0';
-        mapWrapper.style.overflow = 'hidden';
-        mapWrapper.style.height = '333px';
-        mapWrapper.style.filter = 'blur(2px) brightness(1)';
-        mapWrapper.style.marginTop = '71px';
+        map.style.position = 'relative';
+        map.style.top = '0';
+        map.style.left = '0';
+        map.style.overflow = 'hidden';
+        map.style.height = '333px';
+        map.style.filter = 'blur(2px) brightness(1)';
+        map.style.marginTop = '71px';
         width > 375 ? mapImg.style.left = '0px' : mapImg.style.left = '-129px';
         mapImg.style.width = '632px';
         mapImg.style.height = '347px';
+        mapWrapper.style.height = 'unset'; //
+        eighthSection.style.height = 'unset'; //
+        map.style.width = `${mapWidth}`; //
+    };
+
+    const tapToZoomHandler = () => {
+        if (width < 1360) {
+            zoomBtn.addEventListener('click', zoomBtnClickHandler);
+            mapImg.addEventListener('click', mapImgClickHandler);
+        } else {
+            zoomBtn.removeEventListener('click', zoomBtnClickHandler);
+            mapImg.removeEventListener('click', mapImgClickHandler);
+        }
+    };
+
+    const mapSizeHandler = () => {
+        if (width >= 1360) {
+            map.style.position = 'unset';
+            map.style.top = 'unset';
+            map.style.left = 'unset';
+            map.style.overflow = 'unset';
+            map.style.height = 'unset';
+            map.style.filter = 'unset';
+            map.style.marginTop = 'unset';
+            map.style.width = 'unset';
+            mapImg.style.left = 'unset';
+            mapImg.style.width = 'unset';
+            mapImg.style.height = 'unset';
+            zoomBtn.style.display = 'none';
+        } else {
+            map.style.position = 'relative';
+            map.style.top = '0px';
+            map.style.left = '0px';
+            map.style.overflow = 'hidden';
+            map.style.height = '333px';
+            map.style.filter = 'blur(2px) brightness(1);';
+            map.style.marginTop = '71px';
+            map.style.width = '632px';
+            mapImg.style.left = '0px';
+            mapImg.style.width = '632px';
+            mapImg.style.height = '347px';
+            zoomBtn.style.display = 'block';
+        }
+    };
+    tapToZoomHandler();
+    window.addEventListener('resize', () => {
+        width = body.offsetWidth;
+        top = mapWrapper.offsetTop;
+        tapToZoomHandler();
+        mapSizeHandler();
+        mapWidth = window.getComputedStyle(map).width;
     });
 };
 
